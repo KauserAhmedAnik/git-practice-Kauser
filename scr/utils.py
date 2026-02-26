731422f7c2244e3dbd207f88e3ff18637e28ba91
KauserAhmedAnik <kauserahmed22345@gmail.com>
2026-02-26 21:54:22 +0600

    Calculator errors have been handled within a new brance feature/error handling

--- scr/utils.py
+++ scr/utils.py
@@ -2,7 +2,13 @@ def add(a,b):
     return a+b
 
 def subtract(a,b):
-    return a-b
+    if(a <= 0 or a < b):
+        return "There is an error!"
+    else:
+        return a-b
 
 def devision(a,b):
-    return a/b
+    if (a <= 0):
+        return "There can't be devisible value which is devided  by 0 or negative number"
+    else:
+     return a/b
