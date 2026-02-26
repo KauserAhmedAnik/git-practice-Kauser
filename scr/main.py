d36483cbf4b863ed557808f09850196400e7b57f
KauserAhmedAnik <kauserahmed22345@gmail.com>
2026-02-26 21:01:18 +0600

    Add basic calculator functions

--- scr/main.py
+++ scr/main.py
@@ -1,2 +1,8 @@
 print("Kauser Ahmed Anik")
 print(" Todays date is : 26 February 2026")
+
+
+import utils
+
+print(f"Result of addition: {utils.add(8,4)}")
+print(f"Result of subtraction: {utils.subtract(15,6)}")
